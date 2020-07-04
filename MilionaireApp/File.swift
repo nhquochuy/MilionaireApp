b96ae79e8e232996f3192d610760787bdad080bd
QuocHuy <nhquochuy@gmail.com>
2020-07-04 07:20:09 +0700

    Finish Core Source

---
+++ MilionaireApp/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  MilionaireApp
+//
+//  Created by Quoc Huy on 6/29/20.
+//  Copyright © 2020 Quoc Huy. All rights reserved.
+//
+
+import Foundation
---
+++ MilionaireApp/HomeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  HomeViewController.swift
+//  MilionaireApp
+//
+//  Created by Quoc Huy on 6/29/20.
+//  Copyright © 2020 Quoc Huy. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: Initial Control
+    let startButton : UIButton={
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = #colorLiteral(red: 0.003921568627, green: 0.337254902, blue: 0.4078431373, alpha: 1)
+        button.setTitle("START", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 1, green: 0.953854382, blue: 0.8467800021, alpha: 1), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Verdana", size: 30)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        button.layer.cornerRadius = 70
+        button.addTarget(self, action: #selector(StartButtonClick), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func StartButtonClick(){
+        let QuizView = QuizViewController()
+        //navigationController?.pushViewController(QuizView, animated: true)
+        present(QuizView, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 0.953854382, blue: 0.8467800021, alpha: 1)
+        
+        // MARK: Add Subview and Constrains
+        self.view.addSubview(startButton)
+        
+        NSLayoutConstraint.activate([startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor), startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor), startButton.widthAnchor.constraint(equalToConstant: 140), startButton.heightAnchor.constraint(equalToConstant: 140)])
+    }
+}
